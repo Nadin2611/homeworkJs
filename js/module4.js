c1e2cfe7853507b6829b06abf727a411bfcbca4a
Nadin2611 <132609243+Nadin2611@users.noreply.github.com>
2023-08-09 12:57:11 +0300

    module 4

--- js/module4.js
+++ js/module4.js
@@ -1,8 +1,47 @@
 // Exercise 1----------------------------------------------------------------
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+// // Change code below this line
+
+// const result = makePizza();
+// const pointer = makePizza;
 
 // Exercise 2----------------------------------------------------------------
+// Доповни функцію makeMessage таким чином, щоб вона очікувала другим
+// параметром(параметр callback) колбек - функцію і повертала її виклик.
+// Функція deliverPizza або makePizza буде передаватися як колбек і очікувати
+// аргументом ім'я готової піци, що доставляється.
+
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+// Chande code below this line
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+}
+console.log(makeMessage("Royal Grand", makePizza));
+console.log(makeMessage("Ultracheese", deliverPizza));
 
 // Exercise 3----------------------------------------------------------------
+function makePizza(pizzaName, callback) {
+  console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+  callback(pizzaName);
+}
+
+makePizza("Royal Grand", function deliverPizza(pizzaName) {
+  console.log(`Delivering pizza ${pizzaName}.`);
+});
+// Change code below this line
+
+makePizza("Ultracheese", function eatPizza(pizzaName) {
+  console.log(`Eating pizza ${pizzaName}.`);
+});
 
 // Exercise 4----------------------------------------------------------------
 
