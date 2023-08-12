06c25853f3766e6383f5f72a0e16b535f42b7e2a
Nadin2611 <132609243+Nadin2611@users.noreply.github.com>
2023-08-12 17:27:42 +0300

    practic

--- js/practic.js
+++ js/practic.js
@@ -1,26 +1,10 @@
 // дз з практики:
 // Перевірити два масиву і дізнатися, чи вони рівні
 
-const arr1 = [1, 4, 6, "color", 324, 232, "list", 11, 9, "dream", 34, 0, -30];
-const arr2 = [6, "dream", -30, 11, 9, 1, 324, 34, "color", 4, 232, 0, "list"];
+// const arr1 = [1, 4, 6, "color", 324, 232, "list", 11, 9, "dream", 34, 0, -30];
+// const arr2 = [6, "dream", -30, 11, 9, 1, 324, 34, "color", 4, 232, 0, "list"];
 
-function arraysAreEqual(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i += 1) {
-    for (let j = 0; j < arr1.length - i - 1; j += 1) {
-      if (arr1[j + 1] < arr1[j]) {
-        const item = arr1[j];
-        arr1[j] = arr1[j + 1];
-        arr1[j + 1] = item;
-      }
-    }
-  }
-  console.log(arr1);
-}
-console.log(arraysAreEqual(arr1, arr2));
+// console.log(arraysAreEqual(arr1, arr2));
 
 // const arr3 = [4, 232, 6, -30, "color", 324, "list", 1, 11, 9, "dream", 34, 0];
 // const arr4 = ["color", 6, -30, 11, 9, 1, "dream", 324, 34, 4, 232, 0, "list"];
@@ -30,10 +14,55 @@ console.log(arraysAreEqual(arr1, arr2));
 
 // const arr7 = [1, 4, 6, "color", 324, 232, "list", 11, 9, "dream", 34, 0, -30];
 // const arr8 = [6, "dream", -30, 10, 9, 1, 324, 34, "color", 4, 232, 0, "list"];
-//Напишіть функцію caclculateAverage()
+
+// Напишіть функцію caclculateAverage()
 //яка приймає довільну кількість
 //аргументів і повертає їхнє середнє значення.
 //Додати перевірку, що аргументи це числа.
+
+// function calculateAverage() {
+//   if (arguments.length === 0) {
+//     return "немає аргументів";
+//   }
+
+//   let sum = 0;
+//   let countValidArgument = 0;
+
+//   for (let i = 0; i < arguments.length; i += 1) {
+//     if (typeof arguments[i] === "number") {
+//       sum += arguments[i];
+//       countValidArgument += 1;
+//     }
+//   }
+
+//   if (countValidArgument === 0) {
+//     return "жоден аргумент не є числом";
+//   }
+//   const average = sum / countValidArgument;
+//   return average;
+// }
+
+// console.log(calculateAverage(2, 4, 6, 46, 115));
+// console.log(calculateAverage(10, 15, 20, 25, 65, -1, -187));
+// console.log(calculateAverage(1, "not a number", 3));
+// console.log(calculateAverage());
+// console.log(calculateAverage(false, "5", "w", undefined, "summer"));
+
 // Напишіть функцію min(a, b), яка повертає
 // менше з чисел a, b
 // Потрібно додати перевірку, що функція отримує числа
+
+// function min(a, b) {
+//   if (isNaN(a) || isNaN(a)) {
+//     return "not a number";
+//   } else if (a > b) {
+//     return `min: ${b}`;
+//   } else if (a < b) {
+//     return `min: ${a}`;
+//   }
+//   return `a=b`;
+// }
+
+// console.log(min(-2, 3));
+// console.log(min(3, 3));
+// console.log(min("hello", 3));
