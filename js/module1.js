92b3c1f4fd5d80d3a1339568c973a0f4b7477575
Nadin2611 <kyrychenko.nadia07@gmail.com>
2023-08-17 15:38:29 +0300

    Update module1.js

--- js/module1.js
+++ js/module1.js
@@ -79,14 +79,14 @@
 
 // Доповни код функції add таким чином, щоб вона повертала результат додавання значень трьох параметрів: a, b і c.
 
-function add(a, b, c) {
-  return a + b + c;
-}
+// function add(a, b, c) {
+//   return a + b + c;
+// }
 
-add(2, 5, 8);
-add(15, 27, 10);
-add(10, 20, 30);
-add(5, 10, 15);
+// add(2, 5, 8);
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
 
 // TODO Exercise 10----------------------------------------------------------------
 
@@ -95,14 +95,14 @@ add(5, 10, 15);
 // price - ціна товару
 // Доповни код функції таким чином, щоб у змінну message записувався рядок "You picked <product name>, price per item is <product price> credits", де < product name > і < product price > - це значення параметрів name і price.Використовуй синтаксис шаблонних рядків.
 
-function makeMessage(name, price) {
-  const message = `You picked ${name}, price per item is ${price} credits`;
-  return message;
-}
-makeMessage("Radar", 6150);
-console.log(makeMessage("Scanner", 3500));
-console.log(makeMessage("Reactor", 8000));
-console.log(makeMessage("Engine", 4070));
+// function makeMessage(name, price) {
+//   const message = `You picked ${name}, price per item is ${price} credits`;
+//   return message;
+// }
+// makeMessage("Radar", 6150);
+// console.log(makeMessage("Scanner", 3500));
+// console.log(makeMessage("Reactor", 8000));
+// console.log(makeMessage("Engine", 4070));
 
 // TODO Exercise 11----------------------------------------------------------------
 
@@ -122,15 +122,160 @@ console.log(makeMessage("Engine", 4070));
 // console.log(calculateTotalPrice(12, 70));
 
 // TODO Exercise 12----------------------------------------------------------------
+// Функція makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) складає і повертає повідомлення про покупку ремонтних дроїдів. Вона оголошує три параметри, значення яких будуть задаватися під час її виклику.
+
+// orderedQuantity - кількість дроїдів у замовленні
+// pricePerDroid - ціна одного дроїда
+// deliveryFee - вартість доставки
+// Доповни код функції таким чином, щоб вона повертала повідомлення про замовлення у форматі "You ordered droids worth <total price> credits. Delivery (<delivery fee> credits) is included in total price.". Не забудь про ціну доставки в обчисленнях загальної вартості.
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   // Change code below this line
+//   const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+//   const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+
+//   // Change code above this line
+//   return message;
+// }
 
 // TODO Exercise 13----------------------------------------------------------------
+// Функція isAdult оголошує один параметр age (вік), значення якого буде задаватися під час її виклику. Присвой змінній passed вираз перевірки віку користувача на повноліття. Людина вважається повнолітньою у віці 18 років і старше.
+
+// function isAdult(age) {
+//   const passed = age >= 18;
+
+//   return passed;
+// }
+// // console.log(age > passed);
+// console.log(isAdult(20));
+// console.log(isAdult(14));
+// console.log(isAdult(8));
+// console.log(isAdult(37));
 
 // TODO Exercise 14----------------------------------------------------------------
+// Функція isValidPassword (password) перевіряє рівність збереженого і введеного паролів і повертає результат перевірки - буль true або false. Змінна SAVED_PASSWORD зберігає значення попередньо збереженого пароля. Введений пароль передається у параметр password.
+
+// Присвой змінній isMatch вираз перевірки рівності введених і попередньо збережених паролів. Результатом виразу перевірки повинно бути true, якщо значення збігаються, і false - якщо ні.
+
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+//   const isMatch = SAVED_PASSWORD === password;
+
+//   // Change code above this line
+//   return isMatch;
+// }
+// console.log(isValidPassword("mangodab3st"));
+// console.log(isValidPassword("kiwirul3z"));
+// console.log(isValidPassword("jqueryismyjam"));
+
 // TODO Exercise 15----------------------------------------------------------------
+// Додай вираз перевірки повноліття користувача, значення параметра age, в умову для інструкції if.
+// Якщо користувач повнолітній, повинен виконуватися блок if і у змінну message записується рядок "You are an adult".
+// В іншому випадку повинен виконуватися блок else і записується рядок "You are a minor".
+
+// function checkAge(age) {
+//   let message;
+
+//   if (age >= 18) {
+//     // Change this line
+//     message = "You are an adult";
+//   } else {
+//     message = "You are a minor";
+//   }
+
+//   return message;
+// }
+
+// console.log(checkAge(20));
+// console.log(checkAge(8));
+// console.log(checkAge(14));
+// console.log(checkAge(38));
+
 // TODO Exercise 16----------------------------------------------------------------
+// Функція checkStorage(available, ordered) перевіряє можливість оформлення замовлення і повертає повідомлення про результат. Вона оголошує два параметри, значення яких будуть задаватися під час її виклику:
+
+// available - загальна кількість товарів на складі
+// ordered - одиниць товару в замовленні
+// Використовуючи розгалуження, доповни код функції таким чином, що:
+
+// Якщо в замовленні вказане число, яке перевищує кількість товарів на складі, у змінну message записується рядок "Not enough goods in stock!".
+// В іншому випадку записується рядок "Order is processed, our manager will contact you.".
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+//   if (ordered > available) {
+//     message = "Not enough goods in stock!";
+//   } else message = "Order is processed, our manager will contact you.";
+//   // Change code above this line
+//   return message;
+// }
+// console.log(checkStorage(100, 50));
+// console.log(checkStorage(100, 130));
+// console.log(checkStorage(200, 20));
+// console.log(checkStorage(200, 150));
+// console.log(checkStorage(150, 180));
+
 // TODO Exercise 17----------------------------------------------------------------
+// Заміни вирази зі стандартними математичними операторами на комбінований оператор присвоєння з додаванням, відніманням, множенням і діленням.
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+
+// // Change code below this line
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
 // TODO Exercise 18----------------------------------------------------------------
+// Станція з продажу ремонтних дроїдів готова до запуску, залишилося написати програмне забезпечення для відділу продажів.
+
+// Функція makeTransaction(pricePerDroid, orderedQuantity, customerCredits) виконує транзакцію з продажу дроїдів і повертає повідомлення про результат операції. Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
+
+// pricePerDroid - ціна одного дроїда
+// orderedQuantity - кіл-сть замовлених дроїдів
+// customerCredits - сума коштів на рахунку клієнта
+// Доповни її наступним функціоналом:
+
+// Оголоси змінну totalPrice для зберігання загальної суми замовлення і присвой їй вираз розрахунку цієї суми.
+// Додай перевірку, чи зможе клієнт оплатити замовлення:
+// якщо сума до сплати перевищує кількість кредитів на рахунку клієнта, запиши у змінну message рядок "Insufficient funds!";
+// в іншому випадку, відніми суму покупки з рахунку клієнта і запиши у змінну message повідомлення: "You ordered <число> droids, you have <число> credits left".
+
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   // Change code below this line
+//   const totalPrice = pricePerDroid * orderedQuantity;
+
+//   if (totalPrice > customerCredits) {
+//     message = "Insufficient funds!";
+//   } else {
+//     message = `You ordered ${orderedQuantity} droids, you have ${
+//       customerCredits - totalPrice
+//     } credits left`;
+//   }
+
+//   // Change code above this line
+//   return message;
+// }
+
+// console.log(makeTransaction(3000, 5, 23000));
+// console.log(makeTransaction(1000, 3, 15000));
+// console.log(makeTransaction(5000, 10, 8000));
+// console.log(makeTransaction(2000, 8, 10000));
+// console.log(makeTransaction(500, 10, 5000));
+
 // TODO Exercise 19----------------------------------------------------------------
+// Функція checkPassword(password) отримує пароль користувача в параметр password, перевіряє його на збіг з паролем адміністратора у змінній ADMIN_PASSWORD і повертає повідомлення про результат порівняння, яке зберігається у змінній message.
+
+// Якщо значення параметра password дорівнює null, значить користувач скасував операцію і в message записується рядок "Canceled by user!".
+// Якщо значення параметра password збігається зі значенням ADMIN_PASSWORD, у змінну message присвоюється рядок "Welcome!".
+// Якщо жодна з попередніх умов не виконалася, у змінну message записується рядок "Access denied, wrong password!".
+
 // TODO Exercise 20----------------------------------------------------------------
 // TODO Exercise 21----------------------------------------------------------------
 // TODO Exercise 22----------------------------------------------------------------
