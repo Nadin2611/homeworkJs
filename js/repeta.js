0c52bf0d03a0c124e94436e9a4621d28179dbf71
Nadin2611 <kyrychenko.nadia07@gmail.com>
2023-09-20 12:38:40 +0300

    132

--- js/repeta.js
+++
@@ -1,7 +0,0 @@
-import BSN from "bootstrap.native";
-
-const refs = {
-  modal: document.querySelector(".modal"),
-  subscribeBtn: document.querySelector("button[data-subscribe]"),
-};
-console.log(refs);
