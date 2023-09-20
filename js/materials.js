7c57a0ff8cd7c62d3b0d5fca1b2ce6388f662de5
Nadin2611 <kyrychenko.nadia07@gmail.com>
2023-09-20 12:28:35 +0300

    20

--- js/materials.js
+++ js/materials.js
@@ -275,3 +275,304 @@
 
 // console.log(dog); // { name: 'Манго', __proto__: animal }
 // console.log(animal.isPrototypeOf(dog)); // true
+
+// function slugify(title) {
+//   const slug = title.toLowerCase().split(" ").join("-");
+
+//   return slug;
+// }
+
+// console.log(slugify("Arrays for begginers"));
+// console.log(slugify("English for developer"));
+// console.log(slugify("Ten secrets of JavaScript"));
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
+// function filteredArray(arr, elem) {
+//   let newArr = [];
+//   // Змініть код лише під цим рядком
+//   for (let i = 0; i < arr.length; i += 1) {
+//     if (arr[i].indexOf(elem) === -1) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   // Змініть код лише над цим рядком
+//   return newArr;
+// }
+
+// console.log(
+//   filteredArray(
+//     [
+//       [3, 2, 3],
+//       [1, 6, 3],
+//       [3, 13, 26],
+//       [19, 3, 9],
+//     ],
+//     3
+//   )
+// );
+
+// // вкладки – це масив заголовків до кожного сайту, відкритого у вікні
+// const Window = function (tabs) {
+//   this.tabs = tabs; // Ми зберігаємо записи про масив всередині об'єкта
+// };
+
+// // Коли ви об'єднуєте два вікна в одне
+// Window.prototype.join = function (otherWindow) {
+//   this.tabs = this.tabs.concat(otherWindow.tabs);
+//   return this;
+// };
+
+// // Коли ви відкриєте нову вкладку в кінці
+// Window.prototype.tabOpen = function (tab) {
+//   this.tabs.push("new tab"); // Давайте поки відкриємо нову вкладку
+//   return this;
+// };
+
+// // Коли ви закриєте вкладку
+// Window.prototype.tabClose = function (index) {
+//   // Змініть код лише під цим рядком
+
+//   const tabsBeforeIndex = this.tabs.splice(0, index); // Отримайте вкладки перед вкладкою
+//   console.log(tabsBeforeIndex);
+//   const tabsAfterIndex = this.tabs.splice(1); // Отримайте вкладки після вкладки
+//   console.log(tabsAfterIndex);
+//   this.tabs = tabsBeforeIndex.concat(tabsAfterIndex); // З'єднайте їх разом
+
+//   // Змініть код лише над цим рядком
+
+//   return this;
+// };
+
+// // Давайте створимо три вікна браузера
+// const workWindow = new Window([
+//   "GMail",
+//   "Inbox",
+//   "Work mail",
+//   "Docs",
+//   "freeCodeCamp",
+// ]); // Ваша поштова скринька, диск та інші робочі сторінки
+// const socialWindow = new Window([
+//   "FB",
+//   "Gitter",
+//   "Reddit",
+//   "Twitter",
+//   "Medium",
+// ]); // Соціальні сторінки
+// const videoWindow = new Window(["Netflix", "YouTube", "Vimeo", "Vine"]); // Розважальні сайти
+
+// // Тепер виконайте закриття, відкриття вкладки та інші операції
+// const finalTabs = socialWindow
+//   .tabOpen() // Відкрийте нову вкладку для мемів з котами
+//   .join(videoWindow.tabClose(2)) // Закрийте третю вкладку у вікні відео та приєднайтеся
+//   .join(workWindow.tabClose(1).tabOpen());
+// console.log(finalTabs.tabs);
+
+// TODO=========================6==================================
+// // Глобальна змінна
+// const bookList = [
+//   "The Hound of the Baskervilles",
+//   "On The Electrodynamics of Moving Bodies",
+//   "Philosophiæ Naturalis Principia Mathematica",
+//   "Disquisitiones Arithmeticae",
+// ];
+// console.log(bookList);
+// // Змініть код під цим рядком
+// function add(bookList, bookName) {
+//   let newBookList = [...bookList];
+//   newBookList.push(bookName);
+//   return newBookList;
+//   // Змініть код над цим рядком
+// }
+// console.log(bookList);
+// // Змініть код під цим рядком
+// function remove(bookList, bookName) {
+//   let newBookList = [...bookList];
+//   const book_index = newBookList.indexOf(bookName);
+//   if (book_index >= 0) {
+//     newBookList.splice(book_index, 1);
+//     return newBookList;
+
+//     // Змініть код над цим рядком
+//   }
+// }
+
+// console.log(add(bookList, "A Brief History of Time"));
+// console.log(remove(bookList, "On The Electrodynamics of Moving Bodies"));
+// console.log(bookList);
+
+// TODO===========================================================
+// // Глобальна змінна
+// const watchList = [
+//   {
+//     Title: "Inception",
+//     Year: "2010",
+//     Rated: "PG-13",
+//     Released: "16 Jul 2010",
+//     Runtime: "148 min",
+//     Genre: "Action, Adventure, Crime",
+//     Director: "Christopher Nolan",
+//     Writer: "Christopher Nolan",
+//     Actors: "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+//     Plot: "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+//     Language: "English, Japanese, French",
+//     Country: "USA, UK",
+//     Awards: "Won 4 Oscars. Another 143 wins & 198 nominations.",
+//     Poster:
+//       "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//     Metascore: "74",
+//     imdbRating: "8.8",
+//     imdbVotes: "1,446,708",
+//     imdbID: "tt1375666",
+//     Type: "movie",
+//     Response: "True",
+//   },
+//   {
+//     Title: "Interstellar",
+//     Year: "2014",
+//     Rated: "PG-13",
+//     Released: "07 Nov 2014",
+//     Runtime: "169 min",
+//     Genre: "Adventure, Drama, Sci-Fi",
+//     Director: "Christopher Nolan",
+//     Writer: "Jonathan Nolan, Christopher Nolan",
+//     Actors: "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+//     Plot: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+//     Language: "English",
+//     Country: "USA, UK",
+//     Awards: "Won 1 Oscar. Another 39 wins & 132 nominations.",
+//     Poster:
+//       "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+//     Metascore: "74",
+//     imdbRating: "8.6",
+//     imdbVotes: "910,366",
+//     imdbID: "tt0816692",
+//     Type: "movie",
+//     Response: "True",
+//   },
+//   {
+//     Title: "The Dark Knight",
+//     Year: "2008",
+//     Rated: "PG-13",
+//     Released: "18 Jul 2008",
+//     Runtime: "152 min",
+//     Genre: "Action, Adventure, Crime",
+//     Director: "Christopher Nolan",
+//     Writer:
+//       "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+//     Actors: "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+//     Plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+//     Language: "English, Mandarin",
+//     Country: "USA, UK",
+//     Awards: "Won 2 Oscars. Another 146 wins & 142 nominations.",
+//     Poster:
+//       "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+//     Metascore: "82",
+//     imdbRating: "9.0",
+//     imdbVotes: "1,652,832",
+//     imdbID: "tt0468569",
+//     Type: "movie",
+//     Response: "True",
+//   },
+//   {
+//     Title: "Batman Begins",
+//     Year: "2005",
+//     Rated: "PG-13",
+//     Released: "15 Jun 2005",
+//     Runtime: "140 min",
+//     Genre: "Action, Adventure",
+//     Director: "Christopher Nolan",
+//     Writer:
+//       "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+//     Actors: "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+//     Plot: "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+//     Language: "English, Urdu, Mandarin",
+//     Country: "USA, UK",
+//     Awards: "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+//     Poster:
+//       "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+//     Metascore: "70",
+//     imdbRating: "8.3",
+//     imdbVotes: "972,584",
+//     imdbID: "tt0372784",
+//     Type: "movie",
+//     Response: "True",
+//   },
+//   {
+//     Title: "Avatar",
+//     Year: "2009",
+//     Rated: "PG-13",
+//     Released: "18 Dec 2009",
+//     Runtime: "162 min",
+//     Genre: "Action, Adventure, Fantasy",
+//     Director: "James Cameron",
+//     Writer: "James Cameron",
+//     Actors: "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+//     Plot: "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+//     Language: "English, Spanish",
+//     Country: "USA, UK",
+//     Awards: "Won 3 Oscars. Another 80 wins & 121 nominations.",
+//     Poster:
+//       "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+//     Metascore: "83",
+//     imdbRating: "7.9",
+//     imdbVotes: "876,575",
+//     imdbID: "tt0499549",
+//     Type: "movie",
+//     Response: "True",
+//   },
+// ];
+
+// // Змініть код лише під цим рядком
+
+// const ratings = watchList.map((film) => ({
+//   title: film.Title,
+//   rating: film.imdbRating,
+// }));
+
+// // Змініть код лише над цим рядком
+
+// console.log(JSON.stringify(ratings));
+
+// TODO==========================MAP=================================
+// Array.prototype.myMap = function (callback) {
+//   const newArray = [];
+//   // Змініть код лише під цим рядком
+//   for (let i = 0; i < this.length; i += 1) {
+//     newArray.push(callback(this[i], i, this));
+//   }
+//   // Змініть код лише над цим рядком
+//   return newArray;
+// };
+
+// console.log([23, 65, 98, 5, 13].myMap((item) => item * 2));
+
+// const filteredList = watchList
+//   .map((film) => ({
+//     title: film.Title,
+//     rating: film.imdbRating,
+//   }))
+//   .filter((film) => Number(film.rating) > 8.0);
+
+// console.log(filteredList);
+
+// TODO================FILTER===========================================
+
+// Array.prototype.myFilter = function (callback) {
+//   const newArray = [];
+//   // Змініть код лише під цим рядком
+//   for (let i = 0; i < this.length; i += 1) {
+//     if (callback(this[i], i, this)) {
+//       newArray.push(this[i]);
+//     }
+//   }
+//   // Змініть код лише над цим рядком
+//   return newArray;
+// };
+
+function nonMutatingSplice(cities) {
+  return cities.slice(0, 3);
+}
+
+console.log(
+  nonMutatingSplice(["Chicago", "Delhi", "Islamabad", "London", "Berlin"])
+);
